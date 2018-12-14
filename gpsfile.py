7a83994ec29ccb603bb101ea5a52a0b3cee92f7e
mnltake <45388626+mnltake@users.noreply.github.com>
2018-12-14 19:03:58 +0900

    Update gpsfile.py

--- gpsfile.py
+++ gpsfile.py
@@ -2,74 +2,34 @@
 import socket 
 from io import StringIO
 from datetime import datetime
-import shapefile
-from shapely.geometry.point import Point 
-from shapely.geometry import shape 
-host = '127.0.0.1' #localhost
-#host = '192.168.3.20' 
 port = 52002
-bufsize = 256
+bufsize = 150
 buff = StringIO()
-#filepath = '/home/pi/rtklog/'
-#shpfile = '/home/pi/SHP/2019utf_WGS84.shp'
-shpfile = '‪D:/QGIS/2019utf_WGS84.shp'
-filepath = 'D:/rtklog'
+filepath = '../rtklog/'
 #座標取得
 def getpoint():
-    
     buff = StringIO()
     data = sock.recv(bufsize)
-    #print(len(data))
+    print(data)
     buff.write(data.decode('utf-8'))
-    data = buff.getvalue().replace('\n', '')
-    pointlist = data.split()
+    data = buff.getvalue()
+    dlist = data.split()
+    #print(dlist)
+    #print(len(dlist))
     buff.close()
-    if  len(pointlist)  <15  :
-        print("getpoint re-try")
-        getpoint()
-    return pointlist
+    return dlist
 
-def getshp(lon,lat):
-
-    try : 
-        point = ( lon , lat )#（経度lon、緯度lat）
-        print(point)
-        shp = shapefile.Reader(shpfile) #open the shapefile
-        print(type(shp))
-        all_shapes = shp.shapes() 
-        all_records = shp.records()
-
-        for i in range(len(all_shapes)):
-            boundary = all_shapes[i] 
-            if Point(point).within(shape(boundary)): 
-               this_record= all_records[i] [:]
-               print( "圃場データ",this_record[9:11] ) #[9]:圃場コード　[10]:地番名
-               return this_record
-        print("NO SHP DATA")
-        return 0
-    except :
-        print("getshp error")
-        return 0
 try:
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     sock.connect((host, port))
+    now = datetime.now()
+    file = 'TGSlog_{0:%Y%m%d%H%M}.pos'.format(now)
     while True:
-        nowpoint = getpoint()
-        shprecord = getshp(float(nowpoint[3]),float(nowpoint[2]))
-        while shprecord == 0:
-            nowpoint = getpoint()
-            shprecord = getshp(float(nowpoint[3]),float(nowpoint[2]))
-            
-        now = datetime.now()
-        file = '{}_{0:%Y%m%d%H%M}.csv'.format(shprecord[9],now)
-        print(file)
-        while getshp(float(nowpoint[3]),float(nowpoint[2])) != 0 :
-            for i in range(3000):
-                fileobj = open(filepath + file, "a", encoding = "utf-8")
-                savepoint = " ".join(getpoint())
-                fileobj.write(savepoint + "\n")
-                fileobj.close()
-                #print("PointSave")
+        fileobj = open(filepath + file, "a", encoding = "utf-8")
+        savepoint = " ".join(getpoint())
+        fileobj.write(savepoint + "\n")
+        fileobj.close()
+        print("PointSave")
 except socket.error:
     print('socket error')
 
