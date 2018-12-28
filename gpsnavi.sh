e0102425b367086169c5fb49ea4606332066fab3
mnltake <45388626+mnltake@users.noreply.github.com>
2018-12-28 21:39:20 +0900

    gpsnavi-MPU

--- gpsnavi.sh
+++ gpsnavi.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 
-/usr/bin/lxterminal -e python3 /home/pi/gpsnavi/gpsnavi.py
+/usr/bin/lxterminal -e python3 /home/pi/gpsnavi/gpsnavi-MPU.py
+#/usr/bin/lxterminal -e python3 /home/pi/gpsnavi/gpsnavi-auto.py
+#/usr/bin/lxterminal -e python3 /home/pi/gpsnavi/gpsnavi.py
