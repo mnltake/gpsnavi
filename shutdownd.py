0e44fb10d7c72556169f5706eb6f31623ae28c81
mnltake <45388626+mnltake@users.noreply.github.com>
2019-01-23 16:29:51 +0900

    pin変更

--- shutdownd.py
+++ shutdownd.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 """
-Pin　3・5・7・GND
+Pin　7・5・11・GND
 pin5をGNDにつなぐとRaspberryPiが起動する
 http://www.aitendo.com/product/11784
 shutdown-start-reboot
@@ -10,10 +10,10 @@ import os, time
 
 GPIO.setmode(GPIO.BOARD)
 
-# Pin3 : shutdown button
-GPIO.setup(3, GPIO.IN)
-# Pin7 : reboot button
+# Pin7 : shutdown button
 GPIO.setup(7, GPIO.IN, pull_up_down = GPIO.PUD_UP)
+# Pin11 : reboot button
+GPIO.setup(11, GPIO.IN, pull_up_down = GPIO.PUD_UP)
 
 def shutdown(channel):
   os.system("sudo shutdown -h now")
@@ -21,8 +21,8 @@ def shutdown(channel):
 def reboot(channel):
   os.system("sudo reboot")
 
-GPIO.add_event_detect(3, GPIO.FALLING, callback = shutdown, bouncetime = 2000)
-GPIO.add_event_detect(7, GPIO.FALLING, callback = reboot, bouncetime = 2000)
+GPIO.add_event_detect(7, GPIO.FALLING, callback = shutdown, bouncetime = 2000)
+GPIO.add_event_detect(11, GPIO.FALLING, callback = reboot, bouncetime = 2000)
 
 while 1:
   time.sleep(100)
