f65ec937f8e441a65d101c3419351a62d0db315a
mnltake <minolu2003@ybb.ne.jp>
2021-12-31 22:57:24 +0900

    keypad

--- gpsnavi-cli.py
+++ gpsnavi-cli.py
@@ -83,12 +83,12 @@ key_u = 19
 GPIO.setup(key_u,GPIO.IN,pull_up_down=GPIO.PUD_UP)
 
 #キーパッド
-# rpi3A
-# key_y = (40 ,38 ,36 ,32 )
-# key_x = (15 ,18 ,16)
 # reTerminal
-key_y = (37 ,35 ,33 ,31 )
-key_x = (29 ,23 ,21)
+key_y = (40 ,38 ,36 ,32 )
+key_x = (15 ,18 ,16)
+# rpi3A
+# key_y = (37 ,35 ,33 ,31 )
+# key_x = (29 ,23 ,21)
 
 # neopixcel LED strip
 LED_COUNT      = 26     # Number of LED pixels.13
