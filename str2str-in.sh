f6663142e4dd42f704f8aad7ddf5e1f45ae51656
mnltake <minolu2003@ybb.ne.jp>
2022-01-01 10:24:48 +0900

    3d

--- str2str-in.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-cd /home/pi/RTKLIB/app/str2str/gcc/
-sudo ./str2str -in ntrip://guest:guest@52.194.74.83:2101/HIGASHIURA -out serial://ttyAMA0:115200
-#./str2str  -in ntrip://rtk2go.com:2101/Meijo-T-RTCM3 -out serial://ttyUSB0:115200
