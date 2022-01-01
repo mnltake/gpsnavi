c0124e7bbadfaaecb7dcecd892f835c1fd5aa506
mnltake <minolu2003@ybb.ne.jp>
2022-01-01 12:29:19 +0900

    str

--- gpsnavi.sh
+++ gpsnavi.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
-
+cd /home/pi/gpsnavi
+/home/pi/lv_micropython/ports/unix/micropython /home/pi/gpsnavi/userver-navi.py &
 /usr/bin/lxterminal -e sudo python3 /home/pi/gpsnavi/gpsnavi-UBX.py
--- gpsnaviGUI.sh
+++ gpsnaviGUI.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 cd /home/pi/gpsnavi
-DISPLAY=:0 micropython /home/pi/gpsnavi/userver-navi.py &
+DISPLAY=:0 /home/pi/lv_micropython/ports/unix/micropython /home/pi/gpsnavi/userver-navi.py &
 sleep 5
 /usr/bin/lxterminal -e sudo python3 /home/pi/gpsnavi/gpsnavi-cli.py &
