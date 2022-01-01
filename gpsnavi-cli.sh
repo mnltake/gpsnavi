1a0766fd6ff8be5abf3d962e1a5311363cb9ec62
mnltake <minolu2003@ybb.ne.jp>
2022-01-01 13:25:13 +0900

    auto staret ok

--- gpsnavi-cli.sh
+++ gpsnavi-cli.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 cd /home/pi/gpsnavi
 
-sleep 5
-/usr/bin/lxterminal -e sudo python3 /home/pi/gpsnavi/gpsnavi-cli.py&
+sleep 10
+/usr/bin/lxterminal -e sudo python3 /home/pi/gpsnavi/gpsnavi-cli.py
